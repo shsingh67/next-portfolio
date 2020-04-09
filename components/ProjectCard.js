bada9903cc00b95c9cacbaae6b0b5af92b41eb43
sshingh67 <singhsharandeep676@gmail.com>
2020-04-08 17:20:47 -0700

    minor change

---
+++ components/ProjectCard.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+const useStyles = makeStyles({
+  root: {
+      height:"200px"
+  },
+});
+
+const ProjectCard = props => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.children}
+          </Typography>
+        </CardContent>
+     
+      <CardActions>
+        <GitHubIcon/> 
+        <Button size="small" color="primary" href={props.link} target="_blank">
+          GitHub
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default ProjectCard;
