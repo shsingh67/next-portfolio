4f43f0b07e92599a17ded8a8a06ae487e7d17583
sshingh67 <singhsharandeep676@gmail.com>
2020-04-23 03:50:52 -0700

    fixed projec card for mobile view

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -6,14 +6,18 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import GitHubIcon from '@material-ui/icons/GitHub';
-import { Grid } from '@material-ui/core';
+import { Grid, IconButton } from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
-   display: 'flex'
+    display: 'flex',
+    height: '250px'
+  },
+  cardDetails: {
+    flex: 1,
   },
   position: {
-    alignItems: "flex-end"
+    alignItems: "flex-start",
 
   }
 });
@@ -22,23 +26,27 @@ const ProjectCard = props => {
   const classes = useStyles();
 
   return (
-    <Grid item xs={12} md={6}>
+    <Grid item xs={12} md={4}>
       <Card className={classes.root}>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {props.title}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {props.children}
-          </Typography>
-        </CardContent>
-
+        <div className={classes.cardDetails}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {props.title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {props.children}
+            </Typography>
+          </CardContent>
+        </div>
         <CardActions className={classes.position}>
-          <GitHubIcon />
-          <Button size="small" color="primary" href={props.link} target="_blank">
-            GitHub
-        </Button>
+          <IconButton href={props.link} target="_blank">
+            <GitHubIcon />
+
+          </IconButton>
+
+
         </CardActions>
+
       </Card>
     </Grid>
   );
