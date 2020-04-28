1d41ef1bd816969d37ebebfda6661428b0e9bcad
sshingh67 <singhsharandeep676@gmail.com>
2020-04-28 12:17:04 -0700

    removed comments

--- pages/index.js
+++ pages/index.js
@@ -95,9 +95,6 @@ const scrollToRef = (ref) => {
 
 function HideOnScroll(props) {
     const { children, window } = props;
-    // Note that you normally won't need to set the window ref as useScrollTrigger
-    // will default to window.
-    // This is only being set here because the demo is in an iframe.
     const trigger = useScrollTrigger({ target: window ? window() : undefined });
 
     return (
@@ -109,10 +106,6 @@ function HideOnScroll(props) {
 
 HideOnScroll.propTypes = {
     children: PropTypes.element.isRequired,
-    /**
-     * Injected by the documentation to work in an iframe.
-     * You won't need it on your project.
-     */
     window: PropTypes.func,
 };
 
